fca664d4dc5ab999b30fc9ec50e6d87a6615a983
kyuwon53 <lkw2v@naver.com>
2021-11-15 23:46:37 +0900

    [Add] webSocket  연결

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -11,4 +13,9 @@ app.get("/*", (req,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+
+// http , ws 서버 둘 다 작동시킬 수 있다. 
+const wss = new WebSocket.server({ server });
+
+server.listen(3000, handleListen);
