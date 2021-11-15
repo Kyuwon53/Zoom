21b8c42912701dd1af86cb58b0a46fd70cc78c19
kyuwon53 <lkw2v@naver.com>
2021-11-15 23:40:17 +0900

    [Add] handleListen

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ app.get("/*", (req,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000);
+app.listen(3000, handleListen);
