aba25128438373a73780be68f0a5bf66dced3f36
kyuwon53 <lkw2v@naver.com>
2021-11-18 01:59:01 +0900

    [Fix] JSON 형태로 데이터를 전송하라

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,6 +3,11 @@ const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
 socket.addEventListener("open", () => {
   console.log("Connected to server ");
 });
@@ -20,14 +25,14 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
 function handleNickSubmit(event) {
   event.preventDefault();
   const input = nickForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("nickname", input.value));
 }
 
 messageForm.addEventListener("submit", handleSubmit);
