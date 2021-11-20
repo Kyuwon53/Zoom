a62204757b7a1c8429d161c1fb8d437dc804faa0
kyuwon53 <lkw2v@naver.com>
2021-11-21 00:13:12 +0900

    [Add] front에 함수 호출하기

--- src/server.js
+++ src/server.js
@@ -15,7 +15,12 @@ const httpServer = http.createServer(app);
 const wsServer = new SocketIO.Server(httpServer);
 
 wsServer.on("connection", socket => {
-  socket.on("enter_room", (msg) => console.log(msg));
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 /* const sockets = [];
