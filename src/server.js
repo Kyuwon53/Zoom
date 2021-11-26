f20e12951b2a084743cab40389452453efe893cf
kyuwon53 <lkw2v@naver.com>
2021-11-26 22:30:11 +0900

    [Add] 방에 입장과 퇴장을 알려라

--- src/server.js
+++ src/server.js
@@ -39,12 +39,17 @@ wsServer.on("connection", socket => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => 
     socket.to(room).emit("bye", socket.nickname)
     );
+    wsServer.sockets.emit("room_change", publicRooms());
   });
+  socket.on("disconnect", () =>{
+    wsServer.sockets.emit("room_change", publicRooms());
+  })
   socket.on("new_message", (msg, room,done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
